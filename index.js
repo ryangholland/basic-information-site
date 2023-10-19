517296983f9788d5bdc80a589e9cf595a6c99c38
ryangholland <rghcode@gmail.com>
2023-10-18 21:11:01 -0400

    Rewrite app using Express

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+/*
+
+No Express
+
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
+
 
 const PORT = 8080;
 let page404;
@@ -16,7 +21,7 @@ const server = http.createServer((req, res) => {
   fs.readFile(filename, (err, data) => {
     if (err) {
       res.writeHead(404, { "Content-Type": "text/html" });
-      res.write(page404)
+      res.write(page404);
       return res.end();
     }
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -26,3 +31,32 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+*/
+
+// Express
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "/contact-me.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
